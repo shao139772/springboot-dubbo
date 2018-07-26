8cb32de10480450653789aab41e1e558a3275d4a
miaomiao <1223602751@qq.com>
2018-07-26 17:01:34 +0800

    就是测试下提交

--- consumer/src/main/java/com/ubisys/springbootdubbo/controller/UserController.java
+++ consumer/src/main/java/com/ubisys/springbootdubbo/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.ubisys.springbootdubbo.controller;
 
 import com.ubisys.springbootdubbo.service.UserService;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,9 @@ import java.util.Map;
  * Created by cw on 2018/7/26.
  */
 @RestController
+@Slf4j
 public class UserController {
 
-
-    //这个注释是从GitHub上新增的
-    public Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -26,15 +25,18 @@ public class UserController {
 
     /**
      * 新增一个注释
+     *
      * @param name
      * @return
      */
     @GetMapping("/userBuy")
     public Map<String, String> buyTicket(String name) {
+
         Map<String, String> result = new HashMap<String, String>();
 
         result.put("name", name);
         result.put("result", userService.getTicket(name));
+        log.info(result.toString());
         return result;
     }
 
